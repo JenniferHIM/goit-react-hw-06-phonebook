d3f8fa3c7a0aae4c7d04ce4f8b3669b77a6286de
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-07-06 12:46:31 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { Switch, Route, Link } from 'react-router-dom';
+import Counter from './Counter/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <ul>
+      <li>
+        {/* <Link to="/counter">Счётчик</Link> */} Счетчик
+      </li>
+      <li>
+        {/* <Link to="/todos">Заметки</Link> */} Заметки
+      </li>
+    </ul>
+
+  
+           <Counter />
+     
+     
+     
+
+
+  </>
+);
 
 export default App;
